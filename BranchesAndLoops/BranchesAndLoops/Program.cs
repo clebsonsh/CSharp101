fd9e83747566d25ede8fa776dd21839817d2a3de
clebson moura <clebson.sh@gmail.com>
2021-08-21 09:16:04 -0300

    Branches and Loops

---
+++ BranchesAndLoops/BranchesAndLoops/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BranchesAndLoops
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Numbers between 1 and 20 evenly divisible by 3");
+
+      int sum = 0;
+      for (int i = 1; i <= 20; i++)
+      {
+        if (i % 3 == 0)
+        {
+          sum += i;
+        }
+      }
+
+      Console.WriteLine($"The sum is { sum }");
+    }
+  }
+}
