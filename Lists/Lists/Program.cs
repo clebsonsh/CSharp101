419700622f127673e53621a4c258bc56a03d53a7
clebson moura <clebson.sh@gmail.com>
2021-08-21 09:40:49 -0300

    Lists in C#

---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var names = new List<string> { "Clebson", "Ana", "Felipe" };
+      names.Add("Maria");
+      names.Add("Bill");
+      names.Remove("Ana");
+      foreach (string name in names)
+      {
+        Console.WriteLine($"Hello { name.ToUpper() }");
+      }
+    }
+  }
+}
