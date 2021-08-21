bdd48c0e0da8f3920925c9a6a0b602644df907e5
clebson moura <clebson.sh@gmail.com>
2021-08-21 08:23:33 -0300

    Loops in C#

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Loops
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int counter = 0;
+      while (counter < 10)
+      {
+        Console.WriteLine($"While loop - The counter is { counter }");
+        counter++;
+      }
+
+      Console.WriteLine("\n----------------------------------------------\n");
+
+      do
+      {
+        Console.WriteLine($"Do while loop - The counter is { counter }");
+        counter++;
+      } while (counter < 10);
+
+      Console.WriteLine("\n----------------------------------------------\n");
+
+      for (int index = 0; index < 10; index++)
+      {
+        Console.WriteLine($"For loop - The index is { index }");
+      }
+    }
+  }
+}
